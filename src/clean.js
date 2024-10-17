f80f6785a74998101176e8459c2b2b6a986278eb
白保祥 <1643220580@qq.com>
2024-10-17 12:04:05 +0800

    文件修改

--- src/clean.js
+++ src/clean.js
@@ -20,6 +20,11 @@ switch (cpuArch) {
 const p = path.join(__dirname, "..","wintun_dll")
 const file_list = fs.readdirSync(p);
 for (const file of file_list) {
+    if (process.platform === 'win32') {
+        const fullPath = path.join(__dirname, "..","wintun_dll", file);
+        fs.unlinkSync(fullPath);
+        continue;
+    }
     if (!file.includes(cpuArch)) {
         const fullPath = path.join(__dirname, "..","wintun_dll", file);
         fs.unlinkSync(fullPath);
